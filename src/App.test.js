57a4a3825e550981462ea87dd966dc7ede215ab9
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-05-05 20:04:06 +0700

    add dummy data

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-it('renders without crashing', () => {
-  const wrapper = shallow(<App />);
-});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(<Voting pair={pair} />, document.getElementById('root'));
