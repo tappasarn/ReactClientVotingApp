a1b9b4f169ab9c14f19cf8d31b73f961cb5cee82
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-05-06 11:37:51 +0700

    format doc

--- src/components/Vote.test.js
+++ src/components/Vote.test.js
@@ -5,29 +5,29 @@ import { Vote } from './Vote';
 
 describe('Voting', () => {
     it('render a pair of buttons', () => {
-        const wrapper = shallow(<Vote pair={['option1', 'option2']}/>);
+        const wrapper = shallow(<Vote pair={['option1', 'option2']} />);
         expect(wrapper.find('button').length).toBe(2);
     });
     it('should handle click event correctly', () => {
         let voteResult;
         const voteFn = (entry) => voteResult = entry;
 
-        const wrapper = shallow(<Vote pair={['option1', 'option2']} vote={voteFn}/>);
+        const wrapper = shallow(<Vote pair={['option1', 'option2']} vote={voteFn} />);
         wrapper.find('button').at(0).simulate('click');
         expect(voteResult).toBe('option1');
     });
     it('disables buttons when user has voted', () => {
-        const wrapper = shallow(<Vote pair={['option1', 'option2']} hasVoted="Trainspotting"/>);
+        const wrapper = shallow(<Vote pair={['option1', 'option2']} hasVoted="Trainspotting" />);
 
         expect(wrapper.props().children[0].props.disabled).toBe(true);
         expect(wrapper.props().children[1].props.disabled).toBe(true);
     });
 
     it('adds label to the voted entry', () => {
-        const wrapper = shallow(<Vote pair={['option1', 'option2']} hasVoted="option1"/>);
+        const wrapper = shallow(<Vote pair={['option1', 'option2']} hasVoted="option1" />);
 
         expect(wrapper.props().children[0].props.disabled).toBe(true);
         expect(wrapper.props().children[1].props.disabled).toBe(true);
         expect(wrapper.childAt(0).text()).toBe('option1Voted');
-      });
+    });
 });
