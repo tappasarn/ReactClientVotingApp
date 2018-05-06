f2e7b96e7ee477525670091eda58d4780dc22554
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-05-06 12:03:46 +0700

    add routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Voting} from './components/Voting';
+import { Route } from 'react-router';
+import App from './components/App';
+import { Voting } from './components/Voting';
 
 import './index.css';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const routes = <Route component={App}>
+    <Route path="/" component={Voting} />
+</Route>;
+
 // note hasVoted could easily be a state of Voting component in React
 // but with Redux we explicitly want the component to be stateless
 ReactDOM.render(
-    <Voting 
-        pair={pair} 
+    <Voting
+        pair={pair}
         hasVoted="Trainspotting"
         winner="Trainspotting"
     />,
